46b75658992d51c7891408e979c7a8dfae008688
Kleberson Brandão Souza <81385589+kleberson1983@users.noreply.github.com>
2022-08-27 10:41:48 -0300

    Added Dockerfile

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
@@ -7,7 +8,8 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
+    return "Laboratório Pipeline DevOps - Dockerizando"
 
 if __name__ == '__main__':
-    app.run()
+    port = os.getenv('PORT')
+    app.run('0.0.0.0', port=port)
